66ba08d1cf8fec6a335197e0e6b8b034ee04bd29
hamed <hamed2005@gmail.com>
2016-01-11 16:50:40 +0100

    Cor Biplot added

--- dataAnalysis.r
+++ dataAnalysis.r
@@ -1,3 +1,6 @@
+library(plotrix)    ## for biplot
+
+
 ##
 setwd("/home/hamed/KUL/Multivar/Project/HomoTarget")
 ##
@@ -79,14 +82,69 @@ mirna.fa <- factanal(mirna, 2, rotation = "promax", control = list(nstart=10))  
 
 ## Biplot
 
+PCA.biplot<- function(x) {
+        #x is the matrix to biplot, x is numeric, thus variables on ratio or interval scales
+        #x has dimnames(x)[[2]] defined (and eventually dimnames(x)[[1]] defined)
+        xm<-apply(x,2,mean)
+        y<-sweep(x,2,xm)
+        ss<-(t(y)%*%y)
+        s<-ss/(nrow(x)-1)
+        d<-(diag(ss))^(-1/2)
+        e<-diag(d,nrow=ncol(x),ncol=ncol(x))
+        z<-y%*%e
+        r<-t(z)%*%z
+        q<-svd(z)
+        gfd<-((q$d[1])+(q$d[2]))/sum(q$d)
+        gfz<-(((q$d[1])^2)+((q$d[2])^2))/sum((q$d)^2)
+        gfr<-(((q$d[1])^4)+((q$d[2])^4))/sum((q$d)^4)
+        l<-diag(q$d,nrow=ncol(x),ncol=ncol(x))
+        R.B<-q$u        #scores matrix
+        C.B<-q$v%*%l    #loadings
+        #possibility to stretch scores by a scale factor
+        scalefactor<-3.5
+        R.B<-q$u *scalefactor
+        
+        par(mar=c(4,4,4,4),pty='s',oma=c(5,0,0,0),font=2)
+        plot(R.B[ ,1],R.B[ ,2],axes=F,xlim=c(-1,1),ylim=c(-1,1),xlab=' ',ylab=' ',cex=.8)
+        mtext('First component',side=1,line=3,cex=.8)
+        mtext('Second component',side=2,line=3,cex=.8)
+        axis(1,at=c(-1,-.8,-.6,-.4,-.2,0,.2,.4,.6,.8,1),cex=.8)
+        axis(2,at=c(-1,-.8,-.6,-.4,-.2,0,.2,.4,.6,.8,1),cex=.8)
+        box( )
+        
+        #ability to plot rownames on the plot, dimnames(x)[[1]] necessary
+        
+        ###could be uncommented to display obs numbers on plot
+        #text(R.B[,1]-.05,R.B[,2]+.05,as.character(dimnames(x)[[1]]),cex=0.5)
+        points(R.B[,1],R.B[,2],pch=".")
+        
+        
+        points(C.B[,1],C.B[,2],pch=".")
+        text(C.B[,1]-.05,C.B[,2]+.05,as.character(dimnames(x)[[2]]),cex=0.8)
+        
+        ## drawing the arrows
+        for (i in seq(1,nrow(C.B),by=1))
+                arrows(0,0,C.B[i,1],C.B[i,2])
+        
+        #Draw circle unit
+        draw.circle(0,0,1,border='black')
+        
+        ##printing some statistics to output
+        mtext('PCA Biplot',side=1,outer=T,cex=1,line=3)
+        results<-list('correlation matrix'=r,'column effects'=C.B,'row effects'=R.B)
+        cat('The goodness of fit for the correlation matrix is',gfr,'for the centered, standardized design matrix',gfz,'and for the Mahalanobis distances is',gfd,' ') 
+        results
+}
 
-
-## Discriminant Analysis
+PCA.biplot(mirna)
 
 
 
 ## Tree based modelling
 
+
+
+## Discriminant Analysis
 
 
 ## Clustering
